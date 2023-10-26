74be10da0101ef65c1ac2444228abbb75f91937e
Foltifeki <foltifeki@gmail.com>
2023-10-26 12:00:49 +0200

    :3

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.html"],
+  theme: {
+    fontFamily: {
+      'courgette': ['Courgette'],
+      'lobster': ['Lobster'],
+    }
+  },
+  plugins: [],
+}
+
